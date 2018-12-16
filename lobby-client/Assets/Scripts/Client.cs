9368f98a173561f4f78942a290cb11ae2df368ae
nzhul <dobromirivanov1@gmail.com>
2018-12-16 20:16:48 +0200

    add two unity projects. One for server and one for client.

---
+++ lobby-client/Assets/Scripts/Client.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Client : MonoBehaviour
+{
+
+    private const int MAX_USER = 100;
+    private const int PORT = 26000;
+    private const int WEB_PORT = 26001;
+    private const int BYTE_SIZE = 1024;
+
+    private const string SERVER_IP = "127.0.0.1";
+
+    private byte reliableChannel;
+    private int hostId;
+    private byte error;
+
+    private bool isStarted;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Init();
+    }
+
+    private void Update()
+    {
+        UpdatemessagePump();
+    }
+
+    public void Init()
+    {
+        NetworkTransport.Init();
+
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+        this.reliableChannel = connectionConfig.AddChannel(QosType.Reliable);
+
+        HostTopology topo = new HostTopology(connectionConfig, MAX_USER);
+
+        // Client only code
+        this.hostId = NetworkTransport.AddHost(topo, 0);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        // Web Client
+        NetworkTransport.Connect(hostId, SERVER_IP, WEB_PORT, 0, out error);
+        Debug.Log("Connecting from Web");
+#else
+        // Standalone Client
+        NetworkTransport.Connect(hostId, SERVER_IP, PORT, 0, out error);
+        Debug.Log("Connecting from standalone");
+#endif
+
+        Debug.Log(string.Format("Attempting to connect on {0} ...", SERVER_IP));
+
+        isStarted = true;
+    }
+
+    public void Shutdown()
+    {
+        isStarted = false;
+        NetworkTransport.Shutdown();
+    }
+
+    public void UpdatemessagePump()
+    {
+        if (!isStarted)
+        {
+            return;
+        }
+
+        int recievingHostId; // Is this from Web ? Or standalone
+        int connectionId; // Which user is sending me this ?
+        int channelId; // Which lane is he sending that message from
+
+        byte[] recBuffer = new byte[BYTE_SIZE];
+        int dataSize;
+
+        NetworkEventType type = NetworkTransport.Receive(out recievingHostId, out connectionId, out channelId, recBuffer, BYTE_SIZE, out dataSize, out error);
+
+        switch (type)
+        {
+            case NetworkEventType.DataEvent:
+                Debug.Log("Data");
+                break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("We have connected to the server");
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("We have been disconnected");
+                break;
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.BroadcastEvent:
+                Debug.Log("Unexpected network event type!");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ lobby-server/Assets/Scripts/Server.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+
+    private const int MAX_USER = 100;
+    private const int PORT = 26000;
+    private const int WEB_PORT = 26001;
+    private const int BYTE_SIZE = 1024;
+
+    private byte reliableChannel;
+    private int hostId;
+    private int webHostId;
+    private byte error;
+
+    private bool isStarted;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Init();
+    }
+
+    private void Update()
+    {
+        UpdatemessagePump();
+    }
+
+    public void Init()
+    {
+        NetworkTransport.Init();
+
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+        this.reliableChannel = connectionConfig.AddChannel(QosType.Reliable);
+
+        HostTopology topo = new HostTopology(connectionConfig, MAX_USER);
+
+        // SERVER only code
+        this.hostId = NetworkTransport.AddHost(topo, PORT, null);
+        this.webHostId = NetworkTransport.AddWebsocketHost(topo, WEB_PORT, null);
+
+        Debug.Log(string.Format("Opening connection on port {0} and webport {1}", PORT, WEB_PORT));
+
+        isStarted = true;
+    }
+
+    public void Shutdown()
+    {
+        isStarted = false;
+        NetworkTransport.Shutdown();
+    }
+
+    public void UpdatemessagePump()
+    {
+        if (!isStarted)
+        {
+            return;
+        }
+
+        int recievingHostId; // Is this from Web ? Or standalone
+        int connectionId; // Which user is sending me this ?
+        int channelId; // Which lane is he sending that message from
+
+        byte[] recBuffer = new byte[BYTE_SIZE];
+        int dataSize;
+
+        NetworkEventType type = NetworkTransport.Receive(out recievingHostId, out connectionId, out channelId, recBuffer, BYTE_SIZE, out dataSize, out error);
+
+        switch (type)
+        {
+            case NetworkEventType.DataEvent:
+                Debug.Log("Data");
+                break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log(string.Format("User {0} has connected throught host {1}", connectionId, recievingHostId));
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log(string.Format("User {0} has disconnected :(", connectionId));
+                break;
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.BroadcastEvent:
+                Debug.Log("Unexpected network event type!");
+                break;
+            default:
+                break;
+        }
+    }
+}
